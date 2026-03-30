fd67ebd2b19d966cbae6311bb0e1d27a9ac52c2c
gui <purariquezaaaa@gmail.com>
2026-03-30 09:56:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Site carregado");
+function startMusic() {
+  const music = document.getElementById("bgMusic");
+  music.volume = 0.5;
+  music.play();
+
+  document.querySelector(".overlay").style.display = "none";
+}
